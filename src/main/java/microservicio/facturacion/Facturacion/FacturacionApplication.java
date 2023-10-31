3c5f800287191e8198422e00390e5743b5567830
Johandarkness <70907376+Johandarkness@users.noreply.github.com>
2023-10-30 21:10:41 -0500

    Organización FacturacionApplication.java

--- src/main/java/microservicio/facturacion/Facturacion/FacturacionApplication.java
+++ src/main/java/microservicio/facturacion/Facturacion/FacturacionApplication.java
@@ -2,12 +2,18 @@ package microservicio.facturacion.Facturacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "facturacion.Facturacion.repositorio") 
+@EntityScan(basePackages = "facturacion.Facturacion.modelo") 
+@ComponentScan(basePackages = "facturacion.Facturacion") 
 public class FacturacionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacturacionApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FacturacionApplication.class, args);
+    }
+	
 }
